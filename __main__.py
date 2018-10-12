af445b17ca71a48ff165c2a425066e47768e8b27
VirtualJoker <brandenricci@gmail.com>
2018-10-12 17:25:53 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -9,6 +9,7 @@ client = commands.Bot(command_prefix= "!bat")
 @client.event
 async def on_ready():
     print("Batcomputer is online!")
+    await client.change_presence(game=discord.Game(name="Use '!bat help' to see commands"))
 
 tokenFile = open("token.txt","r")
 token = tokenFile.read()
