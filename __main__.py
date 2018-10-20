29d7c03305679d28cb75e1137494b8aabb66e75e
VirtualJoker <brandenricci@gmail.com>
2018-10-20 19:29:48 -0400

    Updated leveling to now be correct!
    fixed a bug in code. Im a noob!

--- __main__.py
+++ __main__.py
@@ -8,13 +8,16 @@ from discord.ext import commands
 Client = discord.Client()
 client = commands.Bot(command_prefix="!bat ")
 
+
 @client.event
 async def on_message(message):
     with open('members.json', 'r') as f:
         users = json.load(f)
 
-    await update_data(users, message.author)
-    await add_exp(users, message.author, message.channel, 1)
+    ##Ignore channel
+    if message.channel.id != '488931494195232769':
+        await update_data(users, message.author)
+        await add_exp(users, message.author, message.channel, 1)
 
     with open('members.json', 'w') as f:
         json.dump(users, f)
@@ -27,22 +30,25 @@ async def on_ready():
     print("Batcomputer is now online!")
     await client.change_presence(game=discord.Game(name="Use '!bat bat' for help"))
 
+
 @client.command()
 async def rules():
     await client.say("Please check the #rules!")
+
 
 @client.command()
 async def report():
     await client.say("Please use this form to make an issue report: https://goo.gl/forms/Cn4p2LM0ci8TBhys1")
 
+
 @client.command()
 async def upcoming():
     await client.say("This is a work in progress, an announcement will be made when this feature is available.")
 
+
 @client.command()
 async def bat():
     await client.say("Commands\nrules: tells you where rules are\nreport: give you a link for the report form\nupcoming: Gives you a list of upcoming events (Not Implemented)")
-
 
 
 async def update_data(users, user):
@@ -62,6 +68,7 @@ async def add_exp(users, user, channel, amount):
     if experience >= level_next:
         await client.send_message(channel, '{} has leveled up to level: {}'.format(user.mention, str(int(level_now + 1))))
         users[user.id]['level'] += 1
+        users[user.id]['experience'] = 0
 
 
 
