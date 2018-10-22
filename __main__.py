09e8d91765b9bcd09fde9bf4692631a612a2d3f1
VirtualJoker <brandenricci@gmail.com>
2018-10-22 13:40:17 -0400

    Added command: xp
    Allows users to see what level they are and how much more xp they need

--- __main__.py
+++ __main__.py
@@ -51,6 +51,21 @@ async def bat():
     await client.say("Commands\nrules: tells you where rules are\nreport: give you a link for the report form\nupcoming: Gives you a list of upcoming events (Not Implemented)")
 
 
+@client.command(pass_context=True)
+async def xp(ctx):
+    user = ctx.message.author
+
+    with open('members.json', 'r') as f:
+        users = json.load(f)
+
+    xp = users[user.id]['experience']
+    level = users[user.id]['level']
+    xpToNext = int((level * 4) - xp)
+
+    await client.say('{} You are currently level {}. You need {} xp to reach level {}.'.format(user.mention, str(level), str(xpToNext), str(int(level + 1))))
+
+
+
 async def update_data(users, user):
     if not user.id in users:
         users[user.id] = {}
@@ -63,7 +78,7 @@ async def add_exp(users, user, channel, amount):
 
     experience = users[user.id]['experience']
     level_now = users[user.id]['level']
-    level_next = int(level_now * 8)
+    level_next = int(level_now * 4)
 
     if experience >= level_next:
         await client.send_message(channel, '{} has leveled up to level: {}'.format(user.mention, str(int(level_now + 1))))
